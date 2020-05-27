400e20be9e2428170daec42ec973e5bdc589f6e5
Rafal001558 <62039524+Rafal001558@users.noreply.github.com>
2020-05-27 19:44:12 +0200

    Add files via upload

---
+++ library.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="library.css">
+</head>
+<body>
+<div class="container">
+<div class="top">
+<a href="#zad1">tabelka_1</a>
+<a href="#zad2">tabelka_2</a>
+<a href="#zad3">tabelka_3</a>
+</div>
+<div class="guziki">
+
+
+</div>
+<div class="tab">
+
+
+
+</div>
+</div>
+</body>
+</html>
